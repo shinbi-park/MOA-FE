a49f2a2fe8b6333c800d644d0c88cf9dd2501e08
Eunseo-jo <90231153+Eunseo-jo@users.noreply.github.com>
2023-04-18 19:30:07 +0900

    feat: add font

--- src/index.js
+++ src/index.js
@@ -1,11 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./index.css";
 import App from "./App";
+import "./index.css";
 import { BrowserRouter } from "react-router-dom";
 import { RecoilRoot } from "recoil";
-
-import { Provider } from "react-redux";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
