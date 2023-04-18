2a26abb2a4e904976a03ef72953adcde5f3fac1f
Eunseo <90231153+Eunseo-jo@users.noreply.github.com>
2023-04-19 00:04:56 +0900

    refactor: MyPage

--- src/App.js
+++ src/App.js
@@ -1,16 +1,12 @@
 import Post from "./Post/Post";
 import Header from "./Header/Header";
 import { Route, Routes } from "react-router-dom";
-import Profile from "./MyPage/Profile";
-import Setting from "./MyPage/Setting";
-import MyActivity from "./MyPage/MyActivity";
-import MyPostList from "./MyPage/MyPostList";
-import Likedlist from "./MyPage/Likedlist";
+import InfoDeatil from "./PostDetail/InfoDetail";
 import SignupPage from "./LogIn/SignUpPage";
 import Home from "./Home/Home";
 import PostDetail from "./PostDetail/PostDetail";
 import PostEdit from "./PostDetail/PostAdmin/PostUpdate/PostEdit";
-import InfoDetail from "./PostDetail/UserInfo/InfoDetail";
+import MyPage from "./MyPage/MyPage";
 
 function App() {
   return (
@@ -20,15 +16,10 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/post" element={<Post />} />
         <Route path="/signup" element={<SignupPage />} />
-        <Route path="/mypage/" element={<Profile />} />
-        <Route path="/mypage/profile" element={<Profile />} />
-        <Route path="/mypage/activity" element={<MyActivity />} />
-        <Route path="/mypage/setting" element={<Setting />} />
-        <Route path="/applicantDetail" element={<InfoDetail />} />
-        <Route path="/mypage/mylist" element={<MyPostList />} />
-        <Route path="/mypage/likedlist" element={<Likedlist />} />
-        <Route path="/detail/:postId" element={<PostDetail />} />
-        <Route path="/edit/:postId" element={<PostEdit />} />
+        <Route path="/mypage" element={<MyPage />} />
+        <Route path="/applicantDetail" element={<InfoDeatil />} />
+        <Route path="/detail" element={<PostDetail />} />
+        <Route path="/edit" element={<PostEdit />} />
       </Routes>
     </>
   );
